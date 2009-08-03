39c5f7142dedee05b771b50aa01f35b93c7b6369
Russell Jones <spam@codeofficer.com>
2009-08-03 02:27:12 -0400

    nuttin

--- test/test_samantha_application.js
+++ test/test_samantha_application.js
@@ -12,25 +12,25 @@
         matches(/^(\d+)\.(\d+)\.(\d+)$/, Samantha.VERSION);
       });
 
-      context('Samantha.Router', {
-        before: function() {
-          this.router = new Samantha.Router(function() {
-            this.random_setting = 1;
-          });
-        }
-      })
-      .should('create a samantha object', function() {
-        defined(this.app, 'route');
-      })
-      .should('set arbitrary settings in the app', function() {
-        equals(this.app.random_setting, 1);
-      })
-      .should('set namespace as random UUID', function() {
-        matches(/^(\d+)-(\d{1,3})$/, this.app.namespace);
-      })
-      .should('initialize empty routes object', function() {
-        isType(this.app.routes, Object);
-      });
+      // context('Samantha.Router', {
+      //   before: function() {
+      //     this.router = new Samantha.Router(function() {
+      //       this.random_setting = 1;
+      //     });
+      //   }
+      // })
+      // .should('create a samantha object', function() {
+      //   defined(this.app, 'route');
+      // })
+      // .should('set arbitrary settings in the app', function() {
+      //   equals(this.app.random_setting, 1);
+      // })
+      // .should('set namespace as random UUID', function() {
+      //   matches(/^(\d+)-(\d{1,3})$/, this.app.namespace);
+      // })
+      // .should('initialize empty routes object', function() {
+      //   isType(this.app.routes, Object);
+      // });
 
       // context('Samantha.Application', 'init', {
       //   before: function() {
