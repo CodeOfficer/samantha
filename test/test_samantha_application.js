41a3a0ae6ea7751c0774d79499346328f874d944
Russell Jones <spam@codeofficer.com>
2009-08-10 07:34:26 -0400

    some basic tests

--- test/test_samantha_application.js
+++ test/test_samantha_application.js
@@ -8,20 +8,32 @@
         isType(Samantha.Core, Function);
         isType(Samantha.EventContext, Function);
         isType(Samantha.Router, Function);
-        isType(Samantha.Mixin, Function);
+        isType(Samantha.Object, Object);
         matches(/^(\d+)\.(\d+)\.(\d+)$/, Samantha.VERSION);
       });
 
-      // context('Samantha.Router', {
-      //   before: function() {
-      //     this.router = new Samantha.Router(function() {
-      //       this.random_setting = 1;
-      //     });
-      //   }
-      // })
-      // .should('create a samantha object', function() {
-      //   defined(this.app, 'route');
-      // })
+      context('Samantha.Application', 'init', {
+        before: function() {
+          this.app1 = new $.samantha(function() { 
+            this.random_setting = true;
+          });
+          this.app2 = new $.samantha(function() { 
+            this.random_setting = true;
+          });
+        }
+      })
+      .should('set arbitrary settings in the app', function() {
+        equals(this.app1.random_setting, true);
+      })
+      .should('set a namespace as a random UUID', function() {
+        matches(/^(\d+)-(\d{1,3})$/, this.app1.namespace);
+      })
+      .should('create unique namespaces for each app', function() {
+        unique = (this.app1.namespace != this.app2.namespace)
+        equals(unique, true);
+      });
+      
+
       // .should('set arbitrary settings in the app', function() {
       //   equals(this.app.random_setting, 1);
       // })
